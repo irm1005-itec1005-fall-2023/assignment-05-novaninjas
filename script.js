016d364b6f9674efa81188102e74c16a715082f4
IbraGoat <ibrahimmohammad@cmail.carleton.ca>
2023-12-02 20:51:38 -0500

    inprogress

--- script.js
+++ script.js
@@ -80,3 +80,126 @@ function inititialise() {
 //
 
 inititialise();
+
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+const blockSize = 10;
+const width = canvas.width / blockSize;
+const height = canvas.height / blockSize;
+
+let score = 0;
+
+let snake = [
+    { x: 5, y: 5 },
+    { x: 4, y: 5 },
+    { x: 3, y: 5 },
+    { x: 2, y: 5 },
+    { x: 1, y: 5 }
+];
+
+let food = { x: 10, y: 10 };
+
+let direction = 'right';
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    ctx.fillStyle = 'red';
+    ctx.fillRect(food.x * blockSize, food.y * blockSize, blockSize, blockSize);
+
+    ctx.fillStyle = 'green';
+    snake.forEach((block) => {
+        ctx.fillRect(block.x * blockSize, block.y * blockSize, blockSize, blockSize);
+    });
+
+    ctx.fillStyle = 'black';
+    ctx.font = '20px Arial';
+    ctx.fillText(`Score: ${score}`, 10, 30);
+}
+
+function move() {
+    const head = { x: snake[0].x, y: snake[0].y };
+
+    switch (direction) {
+        case 'up':
+            head.y--;
+            break;
+        case 'down':
+            head.y++;
+            break;
+        case 'left':
+            head.x--;
+            break;
+        case 'right':
+            head.x++;
+            break;
+    }
+
+    if (head.x === food.x && head.y === food.y) {
+        score++;
+        snake.push({ x: snake[snake.length - 1].x, y: snake[snake.length - 1].y });
+        food = {
+            x: Math.floor(Math.random() * width),
+            y: Math.floor(Math.random() * height)
+        };
+    } else {
+        snake.pop();
+    }
+
+    snake.unshift(head);
+}
+
+function gameOver() {
+    clearInterval(intervalId);
+    const gameOverElement = document.getElementById('game-over');
+    gameOverElement.innerHTML = 'Game Over!';
+    gameOverElement.style.opacity = 1;
+}
+function checkCollision() {
+    const head = snake[0];
+
+    if (head.x < 0 || head.x >= width || head.y < 0 || head.y >= height) {
+        gameOver();
+    }
+
+    for (let i = 1; i < snake.length; i++) {
+        if (head.x === snake[i].x && head.y === snake[i].y) {
+            gameOver();
+        }
+    }
+}
+
+function changeDirection(event) {
+    switch (event.keyCode) {
+        case 37:
+            if (direction !== 'right') {
+                direction = 'left';
+            }
+            break;
+        case 38:
+            if (direction !== 'down') {
+                direction = 'up';
+            }
+            break;
+        case 39:
+            if (direction !== 'left') {
+                direction = 'right';
+            }
+            break;
+        case 40:
+            if (direction !== 'up') {
+                direction = 'down';
+            }
+            break;
+    }
+}
+
+let intervalId = setInterval(() => {
+    move();
+    checkCollision();
+    draw();
+}, 100);
+
+document.addEventListener('keydown', changeDirection);
+
